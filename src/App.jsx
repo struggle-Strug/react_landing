585ad740e42deb14340ccf39816997ab4bf39164
iwayakeisuke <keisuke.waya@gmail.com>
2023-07-28 22:16:59 +0900

    Initial commit to Staging 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+//import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import Assesment from './pages/AssessmentPage';
 import Home from './pages/HomePage';
 import RegisterMember from './pages/RegisterMemberPage';
