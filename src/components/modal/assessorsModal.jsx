8ee582335e101288d024ea1e8dcaa0e96460c794
kitty <tarispltok.tp@gmail.com>
2023-08-17 01:33:23 +0900

    change function

--- src/components/modal/assessorsModal.jsx
+++ src/components/modal/assessorsModal.jsx
@@ -25,7 +25,7 @@ export default function AssessorsModal({ open, title, msg, status, setShowNumofA
   }
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setShowNumofAssessors(false)}>
+      <Dialog as="div" className="relative z-10" onClose={() => {setShowNumofAssessors(false)}}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
