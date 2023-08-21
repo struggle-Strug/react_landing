bd11702436746de8b7962c16e887416b853eeb35
kitty <tarispltok.tp@gmail.com>
2023-08-21 16:11:08 +0900

    remove console

--- src/components/modal/randomConfirmModal.jsx
+++ src/components/modal/randomConfirmModal.jsx
@@ -25,7 +25,7 @@ export default function RandomConfirmModal({ open, title, msg, status, handleNum
   }
   return (
     <Transition.Root show={open} as={Fragment}>
-      <Dialog as="div" className="relative z-10" onClose={setConfirmMakeRandomAssessors(false)}>
+      <Dialog as="div" className="relative z-10" onClose={() =>{setConfirmMakeRandomAssessors(false)}}>
         <Transition.Child
           as={Fragment}
           enter="ease-out duration-300"
