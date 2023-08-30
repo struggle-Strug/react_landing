3dbb666b565d7794e3fd7687444156e6073cd1c7
kitty <tarispltok.tp@gmail.com>
2023-08-31 01:16:45 +0900

    fix terms

--- src/components/modal/agreeCheckModal.jsx
+++ src/components/modal/agreeCheckModal.jsx
@@ -294,7 +294,7 @@ export default function AgreeCheckModal({ open, setOpenAgreeModal, confirmHandle
                   </label>
                   <button
                     type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 mt-3"
+                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline-none mt-3"
                     onClick={() => confirmHandler()}
                     disabled={!clickableButton}
                   >
