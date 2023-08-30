5cbab31d19fe1bb7d50610f6c9f592a15239649d
kitty <tarispltok.tp@gmail.com>
2023-08-30 22:42:53 +0900

    add terms

--- src/components/modal/agreeCheckModal.jsx
+++ src/components/modal/agreeCheckModal.jsx
@@ -94,8 +94,8 @@ export default function AgreeCheckModal({ open, setOpenAgreeModal, confirmHandle
                   </div>
                 </div>
                 <div className="mt-5 sm:mt-6 items-center px-0 text-center text-sm sm:p-24">
-                  <p className='my-3'>本規約に同意することで、「Heart Beat FINDER」サービスの利用契約が成立し、本規約に従いサービスの利用が可能となります。</p>
-                  <label>
+                  <p>本規約に同意することで、「Heart Beat FINDER」サービスの利用契約が成立し、本規約に従いサービスの利用が可能となります。</p>
+                  <label className='my-3'>
                     <input type="checkbox"
                       defaultChecked={clickableButton}
                       onChange={() => setClickableButton(!clickableButton)}
