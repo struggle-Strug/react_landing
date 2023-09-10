731643be0a53a919ec50fd00f109f1d55f85e19d
kitty <tarispltok.tp@gmail.com>
2023-09-10 19:01:36 +0900

    add terms and privacy link

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,8 +3,8 @@ import { NavLink } from 'react-router-dom'
 export default function Footer() {
   return (
     <div className='text-white fixed bottom-0 z-30 w-full flex flex-col	sm:flex-row h-auto bg-main sm:justify-evenly items-center py-[20px]'>
-      <p className='text-xs mr-5'>
-        <NavLink to={"/terms"}>利用規約</NavLink>
+      <p className='text-xs mb-5'>
+        <NavLink to={"/terms"} className={"mr-5"}>利用規約</NavLink>
         <NavLink to={"https://cuoremo.co.jp/privacy/"} target='__blank'>プライバシーポリシー</NavLink>
       </p>
       <p>株式会社CUOREMO</p>
