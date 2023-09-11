38d63005a46ac21eeabbebb40ec95d7f9711ddfa
kitty <tarispltok.tp@gmail.com>
2023-09-11 22:02:45 +0900

    add policy link to terms

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -3,12 +3,12 @@ import { NavLink } from 'react-router-dom'
 export default function Footer() {
   return (
     <div className='text-white fixed bottom-0 z-30 w-full flex flex-col	sm:flex-row h-auto bg-main sm:justify-evenly items-center py-[20px]'>
-      <p className='text-xs mb-5'>
+      <p className='text-xs mb-5 sm:mb-0'>
         <NavLink to={"/terms"} className={"mr-5"}>利用規約</NavLink>
         <NavLink to={"https://cuoremo.co.jp/privacy/"} target='__blank'>プライバシーポリシー</NavLink>
       </p>
       <p>株式会社CUOREMO</p>
-      <p className='text-xs mb-5 hidden sm:invisible sm:block'>
+      <p className='text-xs mb-5 sm:mb-0 hidden sm:invisible sm:block'>
         <NavLink to={"/terms"} className={"mr-5"}>利用規約</NavLink>
         <NavLink to={"https://cuoremo.co.jp/privacy/"} target='__blank'>プライバシーポリシー</NavLink>
       </p>
