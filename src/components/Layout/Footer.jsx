6bbd076f2f425dc35d8382a5a9ee22a05ade8e1a
kitty <tarispltok.tp@gmail.com>
2023-09-15 23:27:41 +0900

    change link position

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function Footer() {
   return (
-    <div className='text-white fixed bottom-0 z-30 w-full flex flex-col	sm:flex-row h-auto bg-main sm:justify-evenly items-center py-[20px]'>
+    <div className='text-white fixed bottom-0 z-30 w-full flex flex-col	sm:flex-row h-auto bg-main sm:justify-between first-letter:items-center py-[20px] px-[40px]'>
       <p className='text-xs mb-5'>
         <NavLink to={"/terms"} className={"mr-5"}>利用規約</NavLink>
         <NavLink to={"https://cuoremo.co.jp/privacy/"} target='__blank'>プライバシーポリシー</NavLink>
