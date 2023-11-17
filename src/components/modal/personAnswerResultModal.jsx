510cefd280321184e2261fc76796282bfee3c4c5
kitty-0315 <tarispltok.tp@gmail.com>
2023-11-17 12:34:12 +0900

    fix 3rd assessment page

--- src/components/modal/personAnswerResultModal.jsx
+++ src/components/modal/personAnswerResultModal.jsx
@@ -3,11 +3,17 @@ import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 
 // eslint-disable-next-line react/prop-types
-export default function EvaluationModal({ open, setOpenAgreeModal }) {
-  
+export default function PersonAnswerResultModal({
+  open,
+  setOpenModal,
+  userAnswers,
+  categories,
+  selectedMember,
+}) {
   const clickHandler = () => {
-    setOpenAgreeModal(false);
+    setOpenModal(false);
   };
+  console.log(userAnswers);
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -36,19 +42,69 @@ export default function EvaluationModal({ open, setOpenAgreeModal }) {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-[#EBFBFF] px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-6xl sm:p-6 border-4 border-main">
-                <div className='h-[40px] mt-2 mr-auto flex justify-end cursor-pointer' onClick={clickHandler}>
-                  <div>
-                    <span className='w-[40px] h-[1px] block bg-black rotate-45'></span>
-                    <span className='w-[40px] h-[1px] block bg-black -rotate-45'></span>
-                  </div>
+                <div className="font-HiraginoKakuGothicProNW3">
+                  {userAnswers !== undefined && (
+                    <div className="flow-root overflow-y-auto bg-[#EBFBFF] py-10 px-5">
+                      <div
+                        className="h-[40px] mt-2 mr-auto flex justify-end cursor-pointer"
+                        onClick={clickHandler}
+                      >
+                        <div>
+                          <span className="w-[40px] h-[1px] block bg-black rotate-45"></span>
+                          <span className="w-[40px] h-[1px] block bg-black -rotate-45"></span>
+                        </div>
+                      </div>
+                      <div className="text-center text-3xl bg-main w-5/6 mx-auto text-white py-4 mb-10">
+                        {selectedMember.received_evaluations_snapshot}{" "}
+                        さんへのアセスメント結果を見る
+                      </div>
+                      <table className="w-full">
+                        <thead className="bg-white py-2">
+                          <tr className="text-center">
+                            <th className="w-[5%]"></th>
+                            <th>設問</th>
+                            <th className="break-keep">回答</th>
+                            <th className="break-keep">会社平均</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                          {categories &&
+                            categories.map((category, idx) => {
+                              const categoryAnswers = userAnswers.filter(
+                                (answers) =>
+                                  answers.quiz_category_name === category
+                              );
+                              return (
+                                <>
+                                  <tr
+                                    className=" p-1 mt-5 text-xl text-main border-b-[0.5px] border-black"
+                                    key={idx}
+                                  >
+                                    <td
+                                      colSpan={4}
+                                      className="pt-12"
+                                    >{`●${category}`}</td>
+                                  </tr>
+                                  {categoryAnswers.map((answer, idx) => (
+                                    <tr key={idx}>
+                                      <td className="py-2">{idx + 1}</td>
+                                      <td>{answer.quiz}</td>
+                                      <td className="text-center">
+                                        {answer.answer}
+                                      </td>
+                                      <td className="text-center">
+                                        {answer.company_answer_avg.toFixed(1)}
+                                      </td>
+                                    </tr>
+                                  ))}
+                                </>
+                              );
+                            })}
+                        </tbody>
+                      </table>
+                    </div>
+                  )}
                 </div>
-                <div className="flex flex-col justify-center items-center">
-                  <div className="max-w-3xl w-full bg-main py-3 flex justify-center items-center text-white text-3xl">山田太郎さんの自己アセスメント 回答結果</div>
-                  <div>
-                    
-                  </div>
-                </div>
-                
               </Dialog.Panel>
             </Transition.Child>
           </div>
