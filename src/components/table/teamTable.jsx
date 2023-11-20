ae75ed288875841976b62fd93d0561178603014a
asuka-inoue <tohasu081026@gmail.com>
2023-11-20 22:39:11 +0900

    チーム登録・編集

--- src/components/table/teamTable.jsx
+++ src/components/table/teamTable.jsx
@@ -83,15 +83,15 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
 
   return (
     <div>
-      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+      <div className="mx-auto sm:px-0 lg:px-8">
         <div className="w-full sm:flex sm:items-center">
           <div className="xl:w-[25%] lg:w-[15%] md:w-[10%]"></div>
           <div className="w-[50%] flex justify-center sm:flex-1 2xl:mr-14">
-            <h1 className="lg:text-4xl md:text-3xl sp:text-2xl font-bold text-gray-900">
+            <h1 className="lg:text-4xl md:text-2xl sp:text-xl font-bold text-gray-900 font-HiraginoKakuGothicProNW6">
               チーム登録・編集
             </h1>
           </div>
-          <div className="w-[25%] mt-4 2xl:ml-16 sm:mt-0 sm:flex-none">
+          <div className="w-[176px] mt-4 2xl:ml-16 sm:mt-0 sm:flex-none sp:w-full sp:w-fit sp:mx-auto">
             <button
               type="button"
               // className="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 disabled:bg-slate-300"
@@ -112,19 +112,20 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
         </div>
       </div>
       <div className="mt-14 flow-root overflow-hidden">
-        <div className="w-11/12 mx-auto">
-          <div className="h-14 bg-main text-white flex justify-between items-center mx-auto mb-4 lg:pl-20 lg:pr-7 md:px-4">
+        <div className="w-full lg:w-11/12 mx-auto">
+          <div className="h-14 bg-main text-white flex justify-between items-center mx-auto mb-4 xl:pl-20 lg:pr-7 md:px-4">
             <p className="text-xs">会社名</p>
-            <p className="xl:text-2xl lg:text-lg md:text-sm">株式会社CUOREMO</p>
-            <div className="flex justify-center items-center">
+            <p className="xl:text-2xl lg:text-lg md:text-sm font-HiraginoKakuGothicProNW6">株式会社CUOREMO</p>
+            <div className="flex justify-center items-center gap-x-1 lg:gap-x-3">
               <p className="text-xs">会社の生産性スコア</p>
-              <input className="w-20 border border-black bg-white flex text-center placeholder:text-xs px-0 py-1 text-black" type="number"
+              <input className="w-10 lg:w-20 border border-black bg-white flex text-center placeholder:text-xs px-0 py-1 text-black font-HiraginoKakuGothicProNW6 lg:text-xl text-base" type="number"
                 min={1}
                 max={10}
                 placeholder="会社の生産性"
                 value={productivity}
                 disabled={subscriptionGlobal}
                 onChange={(e) => handleChangeProductivity(e.target.value)} />
+                <img className="w-5 lg:w-6 block sp:hidden" src="/public/company-edit.png" alt="編集" />
             </div>
           </div>
           <div className="overflow-y-visible	">
@@ -133,7 +134,7 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
                 <tr>
                   <th
                     scope="col"
-                    className="py-3.5 w-2/12 text-xs xl:text-base font-semibold"
+                    className="py-3.5 w-2/12 text-xs font-semibold"
                   >
                     ID
                     {/* <div className="absolute inset-y-0 right-full z-0 w-screen border-b border-b-gray-200" /> */}
@@ -141,7 +142,7 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
                   </th>
                   <th
                     scope="col"
-                    className="hidden py-3.5 w-5/12 text-xs border-l border-[#A0A0A0] xl:text-base font-semibold sm:table-cell"
+                    className="hidden py-3.5 w-5/12 text-xs border-l border-[#A0A0A0] font-semibold sm:table-cell"
                   >
                     チーム名
                   </th>
@@ -153,7 +154,7 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
                   </th>
                   <th
                     scope="col"
-                    className="hidden py-3.5 text-xs xl:text-base font-semibold sm:table-cell"
+                    className="hidden py-3.5 text-xs font-semibold sm:table-cell"
                   >
                     チームの生産性スコア
                   </th>
@@ -166,7 +167,7 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
                   {teams &&
                     teams.map((team, index) => (
                       <tr key={index} className="odd:bg-[#F5F5F5]">
-                        <td className="relative py-4 w-2/12 text-sm lg:text-base font-medium text-gray-900">
+                        <td className="relative py-4 w-2/12 text-xs lg:text-base font-medium text-gray-900">
                           {team.id}
                           {/* <div className="absolute bottom-0 right-full h-px w-screen bg-gray-100" /> */}
                           {/* <div className="absolute bottom-0 left-0 h-px w-screen bg-main" /> */}
@@ -182,17 +183,18 @@ export default function TeamTable({ teams, setShowModal, setTeamToEdit }) {
                             編集
                           </button>
                         </td>
-                        <td className="w-full py-4 text-sm mx-auto lg:text-base text-gray-500 flex items-center justify-center sm:gap-1">
+                        <td className="w-[121px] lg:w-full py-4 text-sm mx-auto lg:text-base text-gray-500 flex items-center justify-center sm:gap-1">
                           <input
                             type="number"
                             defaultValue={team.productivity_team}
                             onChange={(e) => handleProductivityChange(e, team.id)}
-                            className="w-16 border border-black bg-white flex text-center placeholder:text-xs px-0 py-1 text-black"
+                            className="w-16 border border-black bg-white flex text-center placeholder:text-xs px-0 py-1 text-black text-base font-HiraginoKakuGothicProNW6"
                             max={10}
                             min={1}
                             disabled={subscriptionGlobal}
                             placeholder="数値を入力"
                           />
+                          <img className="w-5 block sp:hidden" src="/public/item-edit.png" alt="編集" />
                         </td>
                       </tr>
                     ))}
