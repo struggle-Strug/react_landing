e4e2441c911c17e16acb9395d11340502d7adf07
kitty-0315 <tarispltok.tp@gmail.com>
2023-12-04 16:52:11 +0900

    fix evaluation modal

--- src/components/radarChart/index.jsx
+++ src/components/radarChart/index.jsx
@@ -25,28 +25,29 @@ function RadarChart({ showThirdPerson, scores }) {
   const labels = scores ? scores['1st']['labels'] : null
   const data = scores
     ? labels.map((l, i) => ({
-        subject: labels[i],
-        A: scores["1st"]["scores"][i],
-        B: scores["3rd"]["scores"][i],
-        fullMark: 4,
-      }))
+      subject: labels[i],
+      A: scores["1st"]["scores"][i],
+      B: scores["3rd"]["scores"][i],
+      fullMark: 4,
+    }))
     : [];
 
   return (
     <>
       <EvaluationModal
         open={openModal}
+        category={data.length}
         setOpenAgreeModal={setOpenModal}
       />
       <ResponsiveContainer width="100%" height="100%" className={"font-HiraginoKakuGothicProNW3"}>
         <RadarChartBase cx="45%" cy="50%" outerRadius="70%" data={data} fill="#f3f6f4">
           <PolarGrid />
           <PolarAngleAxis dataKey="subject" tick={<CustomTick />} radius="40%" />
-          <PolarRadiusAxis axisLine={false} angle={30} domain={[0, 4]}/>
+          <PolarRadiusAxis axisLine={false} angle={30} domain={[0, 4]} />
 
           <Radar name="1st" dataKey="A" stroke="#FF0000" fillOpacity={0} strokeWidth={5} max={4} />
           {showThirdPerson && (
-            <Radar name="3rd" dataKey="B" stroke="#0000FF" fill="#8884d8" fillOpacity={0} strokeWidth={5} max={4}/>
+            <Radar name="3rd" dataKey="B" stroke="#0000FF" fill="#8884d8" fillOpacity={0} strokeWidth={5} max={4} />
           )}
         </RadarChartBase>
       </ResponsiveContainer>
--- src/components/radarChart/simpleChart.jsx
+++ src/components/radarChart/simpleChart.jsx
@@ -14,16 +14,16 @@ function SimpleRadarChart({ isFirst, scores }) {
   const handleClickLabel = () => {
     setShowTeamModal(true)
   }
-    const CustomTickBM = ({ payload, x, y, textAnchor }) => (
-      <g transform={`translate(${x},${y})`}>
-        <text fontSize={12} fontWeight={600} y={-10} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
-          {payload.value}
-        </text>
-        <foreignObject x={payload.value.length * (textAnchor === 'end' ? 0 : textAnchor == 'middle' ? '6' : '12')} y={-5} width={15} height={15}>
-          <p className='font-bold border border-black rounded-full w-3 h-3 text-[10px] flex items-center justify-center cursor-pointer' onClick={handleClickLabel}>?</p>
-        </foreignObject>
-      </g>
-    );
+  const CustomTickBM = ({ payload, x, y, textAnchor }) => (
+    <g transform={`translate(${x},${y})`}>
+      <text fontSize={12} fontWeight={600} y={-10} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+        {payload.value}
+      </text>
+      <foreignObject x={payload.value.length * (textAnchor === 'end' ? 0 : textAnchor == 'middle' ? '6' : '12')} y={-5} width={15} height={15}>
+        <p className='font-bold border border-black rounded-full w-3 h-3 text-[10px] flex items-center justify-center cursor-pointer' onClick={handleClickLabel}>?</p>
+      </foreignObject>
+    </g>
+  );
   let data = subjects.map((subject) => ({
     subject: subject,
     A: 0.0,
@@ -37,25 +37,24 @@ function SimpleRadarChart({ isFirst, scores }) {
     }))
   }
 
-
-
   return (
     <>
-    <EvaluationModal
+      <EvaluationModal
         open={showTeamModal}
+        category={data.length}
         setOpenAgreeModal={setShowTeamModal}
       />
-    <ResponsiveContainer width="100%" height="100%">
-      <RadarChartBase cx="50%" cy="50%" outerRadius="80%" data={data} fill="#f3f6f4">
-        <PolarGrid />
-        <PolarAngleAxis dataKey="subject" tick={<CustomTickBM />} radius="40%" />
-        <PolarRadiusAxis axisLine={false} angle={30} domain={[0, 4]} />
-        {isFirst
-          ? <Radar name="1st" dataKey="A" stroke="#FF0000" fillOpacity={0} strokeWidth={4} />
-          : <Radar name="1st" dataKey="A" stroke="#0000FF" fillOpacity={0} strokeWidth={4} />
-        }
-      </RadarChartBase>
-    </ResponsiveContainer>
+      <ResponsiveContainer width="100%" height="100%">
+        <RadarChartBase cx="50%" cy="50%" outerRadius="80%" data={data} fill="#f3f6f4">
+          <PolarGrid />
+          <PolarAngleAxis dataKey="subject" tick={<CustomTickBM />} radius="40%" />
+          <PolarRadiusAxis axisLine={false} angle={30} domain={[0, 4]} />
+          {isFirst
+            ? <Radar name="1st" dataKey="A" stroke="#FF0000" fillOpacity={0} strokeWidth={4} />
+            : <Radar name="1st" dataKey="A" stroke="#0000FF" fillOpacity={0} strokeWidth={4} />
+          }
+        </RadarChartBase>
+      </ResponsiveContainer>
     </>
   );
 }
