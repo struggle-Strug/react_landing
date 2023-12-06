3d1044962e59f32d484cdfe6902545d35abea817
kitty-0315 <tarispltok.tp@gmail.com>
2023-12-07 01:36:47 +0900

    fix axis letters

--- src/components/radarChart/index.jsx
+++ src/components/radarChart/index.jsx
@@ -13,9 +13,40 @@ function RadarChart({ showThirdPerson, scores }) {
 
   const CustomTick = ({ payload, x, y, textAnchor }) => (
     <g transform={`translate(${x},${y})`}>
-      <text fontSize={12} fontWeight={600} y={-10} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
-        {payload.value}
-      </text>
+      {payload.value === '会社ビジョン共感度' ? (
+        <>
+          <text fontSize={10} fontWeight={600} y={-15} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'会社ビジョン'}
+          </text>
+          <text fontSize={10} fontWeight={600} y={-4} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'共感度'}
+          </text>
+        </>
+      ) : payload.value === '個人ビジョン明確度' ? (
+        <>
+          <text fontSize={10} fontWeight={600} y={-15} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'個人ビジョン'}
+          </text>
+          <text fontSize={10} fontWeight={600} y={-4} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'明確度'}
+          </text>
+        </>
+      ) : payload.value === '会社と個人の統合度' ? (
+        <>
+          <text fontSize={10} fontWeight={600} y={-15} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'会社と個人の'}
+          </text>
+          <text fontSize={10} fontWeight={600} y={-4} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {'統合度'}
+          </text>
+        </>
+      ) : (
+        <>
+          <text fontSize={10} fontWeight={600} y={-11} dy={16} fill="#000" onClick={handleClickLabel} textAnchor={textAnchor} style={{ position: 'relative' }}>
+            {payload.value}
+          </text>
+        </>
+      )}
       <foreignObject x={payload.value.length * (textAnchor === 'end' ? 0 : textAnchor == 'middle' ? '5' : '10')} y={-5} width={15} height={15}>
         <p className='font-bold border border-black rounded-full w-3 h-3 text-[10px] flex items-center justify-center cursor-pointer' onClick={handleClickLabel}>?</p>
       </foreignObject>
